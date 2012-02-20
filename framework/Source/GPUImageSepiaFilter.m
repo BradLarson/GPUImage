e8ccea07e0b6c5759d829f404535ebb44e037d79
Brad Larson <larson@sunsetlakesoftware.com>
2012-02-19 18:46:09 -0600

    Merged in Ben Cochran's addition of a color matrix filter, along with the representation of sepia tone as a subset of that.

--- framework/Source/GPUImageSepiaFilter.m
+++ framework/Source/GPUImageSepiaFilter.m
@@ -1,68 +1,23 @@
 #import "GPUImageSepiaFilter.h"
 
-/* Sepia tone fragment shader:
-
-varying highp vec2 textureCoordinate;
-
-uniform sampler2D inputImageTexture;
-uniform lowp float intensity;
-
-void main()
-{
-    lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
-    lowp vec4 outputColor;
-    outputColor.r = (textureColor.r * 0.393) + (textureColor.g * 0.769) + (textureColor.b * 0.189);
-    outputColor.g = (textureColor.r * 0.349) + (textureColor.g * 0.686) + (textureColor.b * 0.168);    
-    outputColor.b = (textureColor.r * 0.272) + (textureColor.g * 0.534) + (textureColor.b * 0.131);
-    
-    gl_FragColor = (intensity * outputColor) + ((1.0 - intensity) * textureColor);
-}
-*/
-
-
-
-NSString *const kGPUImageSepiaFragmentShaderString = 
-@"varying highp vec2 textureCoordinate;\
-\
-uniform sampler2D inputImageTexture;\
-uniform highp float intensity;\
-\
-void main()\
-{\
-    lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);\
-    lowp float grey = dot(textureColor.rgb, vec3(0.299, 0.587, 0.114));\
-    lowp vec4 outputColor = vec4(grey * vec3(1.2, 1.0, 0.8), 1.0);\
-    \
-    gl_FragColor = (intensity * outputColor) + ((1.0 - intensity) * textureColor);\
-}";
-
 @implementation GPUImageSepiaFilter
 
 - (id)init;
 {
-    if (!(self = [super initWithFragmentShaderFromString:kGPUImageSepiaFragmentShaderString]))
+    if (!(self = [super init]))
     {
 		return nil;
     }
     
-    intensityUniform = [filterProgram uniformIndex:@"intensity"];
     self.intensity = 1.0;
+    self.colorMatrix = (GPUMatrix4x4){
+        {0.3588, 0.7044, 0.1368, 0},
+        {0.2990, 0.5870, 0.1140, 0},
+        {0.2392, 0.4696, 0.0912 ,0},
+        {0,0,0,0},
+    };
 
     return self;
-}
-
-#pragma mark -
-#pragma mark Accessors
-
-@synthesize intensity = _intensity;
-
-- (void)setIntensity:(CGFloat)newValue;
-{
-    _intensity = newValue;
-    
-    [GPUImageOpenGLESContext useImageProcessingContext];
-    [filterProgram use];
-    glUniform1f(intensityUniform, _intensity);
 }
 
 @end
