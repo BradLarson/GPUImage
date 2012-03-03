6d76075f93d1639f790f8303bbb7cdbf223d115b
Darshan Patil <dapatil@gmail.com>
2012-03-03 12:16:58 -0500

    Fixed compilation error

--- framework/Source/GPUImageSobelEdgeDetectionFilter.h
+++ framework/Source/GPUImageSobelEdgeDetectionFilter.h
@@ -4,6 +4,8 @@
 {
     GLint intensityUniform, imageWidthFactorUniform, imageHeightFactorUniform;
     BOOL hasOverriddenImageSizeFactor;
+    CGFloat _imageWidthFactor;
+    CGFloat _imageHeightFactor;
 }
 
 // The image width and height factors tweak the appearance of the edges. By default, they match the filter size in pixels
