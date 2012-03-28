acac72301e258bbe51a8e26446b089e86be9109f
Jake Gundersen <fattjake@gmail.com>
2012-03-28 02:19:25 -0600

    Added a Crosshatch + CGA (4 color palette) filter

---
+++ framework/Source/GPUImageCGAGloryFilter.h
@@ -0,0 +1,13 @@
+//
+//  GPUImageCGAGloryFilter.h
+//  Face Esplode
+//
+//  Created by Jacob Gundersen on 3/22/12.
+//  Copyright (c) 2012 Interrobang Software LLC. All rights reserved.
+//
+
+#import "GPUImageFilter.h"
+
+@interface GPUImageCGAGloryFilter : GPUImageFilter
+
+@end
