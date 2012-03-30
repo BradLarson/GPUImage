0a9e627a803e37ae315b27ff93def9222de5ffa4
Jake Gundersen <fattjake@gmail.com>
2012-03-30 02:59:22 -0600

    PolarPixellate Shader based on Vid by Toneburst

---
+++ framework/Source/GPUImagePolarPixellateFilter.h
@@ -0,0 +1,17 @@
+//
+//  GPUImagePolarPixellateFilter.h
+
+
+#import "GPUImageFilter.h"
+
+@interface GPUImagePolarPixellateFilter : GPUImageFilter {
+    GLint centerUniform, pixelSizeUniform;
+}
+
+// The center about which to apply the distortion, with a default of (0.5, 0.5)
+@property(readwrite, nonatomic) CGPoint center;
+// The amount of distortion to apply, from -2.0 to 2.0, with a default of 0.5
+@property(readwrite, nonatomic) CGPoint pixelSize;
+
+
+@end
