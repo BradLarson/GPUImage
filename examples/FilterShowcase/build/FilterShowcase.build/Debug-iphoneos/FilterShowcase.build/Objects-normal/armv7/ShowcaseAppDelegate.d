6c47121a93431b64136cb62be82fbb32b57a4ea9
Josh Holat <jholat@gmail.com>
2012-05-10 09:42:51 -0500

    add build directory of filtershowcase to .gitignore as well

--- examples/FilterShowcase/build/FilterShowcase.build/Debug-iphoneos/FilterShowcase.build/Objects-normal/armv7/ShowcaseAppDelegate.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /opt/local/www/GPUImage/examples/FilterShowcase/FilterShowcase/ShowcaseAppDelegate.m \
-  /opt/local/www/GPUImage/examples/FilterShowcase/FilterShowcase/ShowcaseAppDelegate.h \
-  /opt/local/www/GPUImage/examples/FilterShowcase/FilterShowcase/ShowcaseFilterListController.h
--- examples/FilterShowcase/build/FilterShowcase.build/Debug-iphoneos/FilterShowcase.build/Objects-normal/armv7/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /opt/local/www/GPUImage/examples/FilterShowcase/FilterShowcase/main.m \
-  /opt/local/www/GPUImage/examples/FilterShowcase/FilterShowcase/ShowcaseAppDelegate.h
