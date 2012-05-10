b97e322b5bcd79d3ce528afca8cbfe9003e68447
Josh Holat <jholat@gmail.com>
2012-05-10 10:58:55 -0500

    Add blurColor ability to change blur darker if needed

--- framework/Source/GPUImageGaussianSelectiveBlurFilter.h
+++ framework/Source/GPUImageGaussianSelectiveBlurFilter.h
@@ -1,15 +1,17 @@
-#import "GPUImageFilterGroup.h"
+#import "GPUImageGaussianBlurFilter.h"
 
-@class GPUImageGaussianBlurFilter;
-
-@interface GPUImageGaussianSelectiveBlurFilter : GPUImageFilterGroup 
-{
-    GPUImageGaussianBlurFilter *blurFilter;
-    GPUImageFilter *selectiveFocusFilter;
+@interface GPUImageGaussianSelectiveBlurFilter : GPUImageGaussianBlurFilter {
+    GLint verticalExcludeCircleRadiusUniform,
+    verticalExcludeCirclePointUniform,
+    verticalExcludeCircleBlurSizeUniform,
+    blurColorUniform;
+    
+    GLuint originalInputImageTexture;
 }
 
 @property (readwrite, nonatomic) CGFloat excludeCircleRadius;
 @property (readwrite, nonatomic) CGPoint excludeCirclePoint;
 @property (readwrite, nonatomic) CGFloat excludeBlurSize;
+@property (readwrite, nonatomic) CGFloat blurColor;
 
 @end
