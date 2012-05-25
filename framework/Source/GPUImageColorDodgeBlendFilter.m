6361d93af20f4ae39044101baa8a5b71d1e1975b
Brad Larson <larson@sunsetlakesoftware.com>
2012-05-25 14:13:57 -0500

    Fixed a precision issue and some divide-by-zero problems with the color dodge blend.

--- framework/Source/GPUImageColorDodgeBlendFilter.m
+++ framework/Source/GPUImageColorDodgeBlendFilter.m
@@ -3,7 +3,7 @@
 NSString *const kGPUImageColorDodgeBlendFragmentShaderString = SHADER_STRING
 ( 
  
- precision lowp float;
+ precision mediump float;
  
  varying highp vec2 textureCoordinate;
  varying highp vec2 textureCoordinate2;
@@ -20,7 +20,7 @@ NSString *const kGPUImageColorDodgeBlendFragmentShaderString = SHADER_STRING
      vec3 rightHandProduct = overlay.rgb * (1.0 - base.a) + base.rgb * (1.0 - overlay.a);
      
      vec3 firstBlendColor = baseOverlayAlphaProduct + rightHandProduct;
-     vec3 overlayRGB = (overlay.rgb / overlay.a) * step(0.0, overlay.a);
+     vec3 overlayRGB = clamp((overlay.rgb / clamp(overlay.a, 0.01, 1.0)) * step(0.0, overlay.a), 0.0, 0.99);
      
      vec3 secondBlendColor = (base.rgb * overlay.a) / (1.0 - overlayRGB) + rightHandProduct;
      
