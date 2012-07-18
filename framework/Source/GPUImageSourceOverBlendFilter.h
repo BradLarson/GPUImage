dd5fd0c4659b5db3b1a64d6e2f32d2d9bc50c572
Hugues Lismonde <hugues@epic.net>
2012-07-18 15:51:40 +0200

    Source over blend filter

---
+++ framework/Source/GPUImageSourceOverBlendFilter.h
@@ -0,0 +1,5 @@
+#import "GPUImageFilter.h"
+
+@interface GPUImageSourceOverBlendFilter : GPUImageFilter
+
+@end
---
+++ framework/Source/GPUImageSourceOverBlendFilter.m
@@ -0,0 +1,31 @@
+#import "GPUImageSourceOverBlendFilter.h"
+
+NSString *const kGPUImageSourceOverBlendFragmentShaderString = SHADER_STRING
+(
+ varying highp vec2 textureCoordinate;
+ 
+ uniform sampler2D inputImageTexture;
+ uniform sampler2D inputImageTexture2;
+ 
+ void main()
+ {
+   lowp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+   lowp vec4 textureColor2 = texture2D(inputImageTexture2, textureCoordinate);
+   
+   gl_FragColor = mix(textureColor, textureColor2, textureColor2.a);
+ }
+);
+
+@implementation GPUImageSourceOverBlendFilter
+
+- (id)init;
+{
+  if (!(self = [super initWithFragmentShaderFromString:kGPUImageSourceOverBlendFragmentShaderString]))
+  {
+		return nil;
+  }
+  
+  return self;
+}
+
+@end
