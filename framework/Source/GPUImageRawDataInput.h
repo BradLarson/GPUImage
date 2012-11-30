fd6765db1a48f73aad4b12b874cdba80afa8fdb5
Mitch <mm@mm.com>
2012-11-29 18:05:59 -0800

    Add a method to allow us to specify the data format in GPUImageRawDataInput.  Right now we support unsigned byte and float.  Also allow for RGB images.

--- framework/Source/GPUImageRawDataInput.h
+++ framework/Source/GPUImageRawDataInput.h
@@ -3,11 +3,18 @@
 // The bytes passed into this input are not copied or retained, but you are free to deallocate them after they are used by this filter.
 // The bytes are uploaded and stored within a texture, so nothing is kept locally.
 // The default format for input bytes is GPUPixelFormatBGRA, unless specified with pixelFormat:
+// The default type for input bytes is GPUPixelTypeUByte, unless specified with pixelType:
 
 typedef enum {
 	GPUPixelFormatBGRA = GL_BGRA,
-	GPUPixelFormatRGBA = GL_RGBA
+	GPUPixelFormatRGBA = GL_RGBA,
+	GPUPixelFormatRGB = GL_RGB
 } GPUPixelFormat;
+
+typedef enum {
+	GPUPixelTypeUByte = GL_UNSIGNED_BYTE,
+	GPUPixelTypeFloat = GL_FLOAT
+} GPUPixelType;
 
 @interface GPUImageRawDataInput : GPUImageOutput
 {
@@ -19,10 +26,12 @@ typedef enum {
 // Initialization and teardown
 - (id)initWithBytes:(GLubyte *)bytesToUpload size:(CGSize)imageSize;
 - (id)initWithBytes:(GLubyte *)bytesToUpload size:(CGSize)imageSize pixelFormat:(GPUPixelFormat)pixelFormat;
+- (id)initWithBytes:(GLubyte *)bytesToUpload size:(CGSize)imageSize pixelFormat:(GPUPixelFormat)pixelFormat type:(GPUPixelType)pixelType;
 
 /** Input data pixel format
  */
 @property (readwrite, nonatomic) GPUPixelFormat pixelFormat;
+@property (readwrite, nonatomic) GPUPixelType   pixelType;
 
 // Image rendering
 - (void)updateDataFromBytes:(GLubyte *)bytesToUpload size:(CGSize)imageSize;
