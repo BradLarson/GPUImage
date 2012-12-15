6016767cd2aff47b82ff49217d91d7a5fc88b855
Jeff Johnson <jjxtra@gmail.com>
2012-12-15 15:34:17 -0700

    Add GPUGaussianBlurPositionFilter and restore the original code for GPUGaussianBlurFilter

--- framework/Source/GPUImageGaussianBlurPositionFilter.m
+++ framework/Source/GPUImageGaussianBlurPositionFilter.m
@@ -10,7 +10,6 @@ NSString *const kGPUImageGaussianBlurVertexShaderString = SHADER_STRING
  uniform highp float texelWidthOffset; 
  uniform highp float texelHeightOffset;
  uniform highp float blurSize;
- 
  varying highp vec2 textureCoordinate;
  varying highp vec2 blurCoordinates[GAUSSIAN_SAMPLES];
  
@@ -40,27 +39,49 @@ NSString *const kGPUImageGaussianBlurFragmentShaderString = SHADER_STRING
  
  varying highp vec2 textureCoordinate;
  varying highp vec2 blurCoordinates[GAUSSIAN_SAMPLES];
+
+ uniform highp float aspectRatio;
+ uniform lowp vec2 blurCenter;
+ uniform highp float blurRadius;
  
  void main() {
- 	lowp vec4 sum = vec4(0.0);
- 	
-     sum += texture2D(inputImageTexture, blurCoordinates[0]) * 0.05;
-     sum += texture2D(inputImageTexture, blurCoordinates[1]) * 0.09;
-     sum += texture2D(inputImageTexture, blurCoordinates[2]) * 0.12;
-     sum += texture2D(inputImageTexture, blurCoordinates[3]) * 0.15;
-     sum += texture2D(inputImageTexture, blurCoordinates[4]) * 0.18;
-     sum += texture2D(inputImageTexture, blurCoordinates[5]) * 0.15;
-     sum += texture2D(inputImageTexture, blurCoordinates[6]) * 0.12;
-     sum += texture2D(inputImageTexture, blurCoordinates[7]) * 0.09;
-     sum += texture2D(inputImageTexture, blurCoordinates[8]) * 0.05;
+     highp vec2 textureCoordinateToUse = vec2(textureCoordinate.x, (textureCoordinate.y * aspectRatio + 0.5 - 0.5 * aspectRatio));
+     highp float dist = distance(blurCenter, textureCoordinateToUse);
 
- 	gl_FragColor = sum;
+     if (dist < blurRadius)
+     {
+        lowp vec4 sum = vec4(0.0);
+        
+         sum += texture2D(inputImageTexture, blurCoordinates[0]) * 0.05;
+         sum += texture2D(inputImageTexture, blurCoordinates[1]) * 0.09;
+         sum += texture2D(inputImageTexture, blurCoordinates[2]) * 0.12;
+         sum += texture2D(inputImageTexture, blurCoordinates[3]) * 0.15;
+         sum += texture2D(inputImageTexture, blurCoordinates[4]) * 0.18;
+         sum += texture2D(inputImageTexture, blurCoordinates[5]) * 0.15;
+         sum += texture2D(inputImageTexture, blurCoordinates[6]) * 0.12;
+         sum += texture2D(inputImageTexture, blurCoordinates[7]) * 0.09;
+         sum += texture2D(inputImageTexture, blurCoordinates[8]) * 0.05;
+
+        gl_FragColor = sum;
+     }
+     else
+     {
+         gl_FragColor = texture2D(inputImageTexture, textureCoordinate);
+     }
  }
 );
+
+@interface GPUImageGaussianBlurFilter ()
+
+@property (readwrite, nonatomic) CGFloat aspectRatio;
+
+@end
 
 @implementation GPUImageGaussianBlurFilter
 
 @synthesize blurSize = _blurSize;
+@synthesize blurCenter = _blurCenter;
+@synthesize aspectRatio = _aspectRatio;
 
 - (id) initWithFirstStageVertexShaderFromString:(NSString *)firstStageVertexShaderString 
              firstStageFragmentShaderFromString:(NSString *)firstStageFragmentShaderString 
@@ -76,8 +97,13 @@ NSString *const kGPUImageGaussianBlurFragmentShaderString = SHADER_STRING
     
     horizontalBlurSizeUniform = [filterProgram uniformIndex:@"blurSize"];
     verticalBlurSizeUniform = [secondFilterProgram uniformIndex:@"blurSize"];
+    aspectRatioUniform = [secondFilterProgram uniformIndex:@"aspectRatio"];
+    blurCenterUniform = [secondFilterProgram uniformIndex:@"blurCenter"];
+    blurRadiusUniform = [secondFilterProgram uniformIndex:@"blurRadius"];
 
     self.blurSize = 1.0;
+    self.blurRadius = 1.0;
+    self.blurCenter = CGPointMake(0.5, 0.5);
     
     return self;
 }
@@ -90,13 +116,53 @@ NSString *const kGPUImageGaussianBlurFragmentShaderString = SHADER_STRING
                       secondStageFragmentShaderFromString:nil];
 }
 
+- (void)setInputSize:(CGSize)newSize atIndex:(NSInteger)textureIndex;
+{
+    CGSize oldInputSize = inputTextureSize;
+    [super setInputSize:newSize atIndex:textureIndex];
+
+    if ( (!CGSizeEqualToSize(oldInputSize, inputTextureSize)) && (!CGSizeEqualToSize(newSize, CGSizeZero)) )
+    {
+        if (GPUImageRotationSwapsWidthAndHeight(inputRotation))
+        {
+            [self setAspectRatio:(inputTextureSize.width / inputTextureSize.height)];
+        }
+        else
+        {
+            [self setAspectRatio:(inputTextureSize.height / inputTextureSize.width)];
+        }
+    }
+}
+
 #pragma mark Getters and Setters
 
-- (void) setBlurSize:(CGFloat)blurSize {
+- (void) setBlurSize:(CGFloat)blurSize;
+{
     _blurSize = blurSize;
 
     [self setFloat:_blurSize forUniform:horizontalBlurSizeUniform program:filterProgram];
     [self setFloat:_blurSize forUniform:verticalBlurSizeUniform program:secondFilterProgram];
+}
+
+- (void) setBlurCenter:(CGPoint)blurCenter;
+{
+    _blurCenter = blurCenter;
+
+    [self setPoint:_blurCenter forUniform:blurCenterUniform program:secondFilterProgram];
+}
+
+- (void) setBlurRadius:(CGFloat)blurRadius;
+{
+    _blurRadius = blurRadius;
+
+    [self setFloat:_blurRadius forUniform:blurRadiusUniform program:secondFilterProgram];
+}
+
+- (void) setAspectRatio:(CGFloat)newValue;
+{
+    _aspectRatio = newValue;
+
+    [self setFloat:_aspectRatio forUniform:aspectRatioUniform program:secondFilterProgram];
 }
 
 @end
