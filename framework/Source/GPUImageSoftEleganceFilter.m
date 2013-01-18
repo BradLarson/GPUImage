6ad440ebcf61e31cb92d7fec5414275ecb2241fb
Cameron Perry <cameron@pivotalaction.com>
2013-01-18 10:54:33 -0800

    Fix defects as described in issue #654 - filters using a lookup filter would not update with new input.

--- framework/Source/GPUImageSoftEleganceFilter.m
+++ framework/Source/GPUImageSoftEleganceFilter.m
@@ -50,6 +50,12 @@
     return self;
 }
 
+-(void)prepareForImageCapture {
+    [lookupImageSource1 processImage];
+    [lookupImageSource2 processImage];
+    [super prepareForImageCapture];
+}
+
 #pragma mark -
 #pragma mark Accessors
 
