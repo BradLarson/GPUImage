8221cdf18a26b394757a6ab749021f71eeb847b6
Ian Simon <biriba@gmail.com>
2013-01-30 13:12:16 -0800

    added comment about number of poisson iterations

--- framework/Source/GPUImagePoissonBlendFilter.h
+++ framework/Source/GPUImagePoissonBlendFilter.h
@@ -21,6 +21,7 @@
 @property(readwrite, nonatomic) CGFloat mix;
 
 // The number of times to propagate the gradients.
+// Crank this up to 100 or even 1000 if you want to get anywhere near convergence.  Yes, this will be slow.
 @property(readwrite, nonatomic) NSUInteger numIterations;
 
 @end
