b958419f03ba1510653f100f28d6b98fb37a064a
Martin Kiss <martin.kiss@me.com>
2013-02-07 14:31:33 +0100

    Add `finishRecording` method with completion block (with backward compatibility)
    iOS 6 SDK and higher: If writer responds to async method, call it, otherwise call synchronous method and completion handler.
    iOS 5 SDK and lower: Call only synchronous method and completion handler. The rest should not be even compiled.

--- framework/Source/GPUImageMovieWriter.h
+++ framework/Source/GPUImageMovieWriter.h
@@ -55,6 +55,7 @@ extern NSString *const kGPUImageColorSwizzlingFragmentShaderString;
 - (void)startRecording;
 - (void)startRecordingInOrientation:(CGAffineTransform)orientationTransform;
 - (void)finishRecording;
+- (void)finishRecordingWithCompletionHandler:(void (^)(void))handler;
 - (void)cancelRecording;
 - (void)processAudioBuffer:(CMSampleBufferRef)audioBuffer;
 - (void)enableSynchronizationCallbacks;
