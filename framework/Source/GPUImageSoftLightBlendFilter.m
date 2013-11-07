49fcf22d00910f5439392881839cf1d6fed729c1
Brad Larson <larson@sunsetlakesoftware.com>
2013-11-07 09:18:58 -0600

    Removed a divide-by-zero error in the soft light blend, caused by accounting for premultiplied alpha.

--- framework/Source/GPUImageSoftLightBlendFilter.m
+++ framework/Source/GPUImageSoftLightBlendFilter.m
@@ -14,7 +14,8 @@ NSString *const kGPUImageSoftLightBlendFragmentShaderString = SHADER_STRING
      mediump vec4 base = texture2D(inputImageTexture, textureCoordinate);
      mediump vec4 overlay = texture2D(inputImageTexture2, textureCoordinate2);
      
-     gl_FragColor = base * (overlay.a * (base / base.a) + (2.0 * overlay * (1.0 - (base / base.a)))) + overlay * (1.0 - base.a) + base * (1.0 - overlay.a);
+     lowp float alphaDivisor = base.a + step(base.a, 0.0); // Protect against a divide-by-zero blacking out things in the output
+     gl_FragColor = base * (overlay.a * (base / alphaDivisor) + (2.0 * overlay * (1.0 - (base / alphaDivisor)))) + overlay * (1.0 - base.a) + base * (1.0 - overlay.a);
  }
 );
 #else
@@ -31,7 +32,8 @@ NSString *const kGPUImageSoftLightBlendFragmentShaderString = SHADER_STRING
      vec4 base = texture2D(inputImageTexture, textureCoordinate);
      vec4 overlay = texture2D(inputImageTexture2, textureCoordinate2);
      
-     gl_FragColor = base * (overlay.a * (base / base.a) + (2.0 * overlay * (1.0 - (base / base.a)))) + overlay * (1.0 - base.a) + base * (1.0 - overlay.a);
+     float alphaDivisor = base.a + step(base.a, 0.0); // Protect against a divide-by-zero blacking out things in the output
+     gl_FragColor = base * (overlay.a * (base / alphaDivisor) + (2.0 * overlay * (1.0 - (base / alphaDivisor)))) + overlay * (1.0 - base.a) + base * (1.0 - overlay.a);
  }
 );
 #endif
