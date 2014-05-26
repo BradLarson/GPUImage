a850610c3b6e22b82836d04df891a95847a8e113
Brent Gulanowski <brent@shopify.com>
2014-05-25 23:17:55 -0600

    Disable noisy logging.

--- framework/Source/GPUImageHarrisCornerDetectionFilter.m
+++ framework/Source/GPUImageHarrisCornerDetectionFilter.m
@@ -221,7 +221,7 @@ NSString *const kGPUImageHarrisCornerDetectionFragmentShaderString = SHADER_STRI
     
     glReadPixels(0, 0, (int)imageSize.width, (int)imageSize.height, GL_RGBA, GL_UNSIGNED_BYTE, rawImagePixels);
 
-    CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
+//    CFAbsoluteTime startTime = CFAbsoluteTimeGetCurrent();
 
     unsigned int imageWidth = imageSize.width * 4;
     
@@ -246,8 +246,8 @@ NSString *const kGPUImageHarrisCornerDetectionFragmentShaderString = SHADER_STRI
         currentByte +=4;
     }
     
-    CFAbsoluteTime currentFrameTime = (CFAbsoluteTimeGetCurrent() - startTime);
-    NSLog(@"Processing time : %f ms", 1000.0 * currentFrameTime);
+//    CFAbsoluteTime currentFrameTime = (CFAbsoluteTimeGetCurrent() - startTime);
+//    NSLog(@"Processing time : %f ms", 1000.0 * currentFrameTime);
 
     if (cornersDetectedBlock != NULL)
     {
