6909af716be1a4934ee520730b023a947abfa9e2
Darren Ehlers <me@darrenehlers.com>
2014-06-02 12:06:40 -0500

    Added code changes to alleviate crash
    Signed-off-by: Darren Ehlers <me@darrenehlers.com>

--- framework/Source/GPUImageFilter.m
+++ framework/Source/GPUImageFilter.m
@@ -189,11 +189,16 @@ NSString *const kGPUImagePassthroughFragmentShaderString = SHADER_STRING
         return NULL;
     }
 
+    // All image output is now managed by the framebuffer itself
+    GPUImageFramebuffer*    framebuffer = [self framebufferForOutput];
+
     usingNextFrameForImageCapture = NO;
     dispatch_semaphore_signal(imageCaptureSemaphore);
     
     // All image output is now managed by the framebuffer itself
-    return [[self framebufferForOutput] newCGImageFromFramebufferContents];
+    CGImageRef  image = [framebuffer newCGImageFromFramebufferContents];
+    return image;
+    //return [[self framebufferForOutput] newCGImageFromFramebufferContents];
 }
 
 #pragma mark -
