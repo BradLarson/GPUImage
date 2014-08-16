bdf73ee97cd353c0ae7143809aea33beb7abf464
Sascha Hameister <hameister@mac.com>
2014-08-16 21:47:48 +0200

    Fixed issue, when camera does not properly started capturing, after viewing the view again in fully glory.

--- examples/iOS/FilterShowcase/FilterShowcase/ShowcaseFilterViewController.m
+++ examples/iOS/FilterShowcase/FilterShowcase/ShowcaseFilterViewController.m
@@ -49,6 +49,14 @@
 	[super viewWillDisappear:animated];
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    // Note: I needed to start camera capture after the view went on the screen, when a partially transition of navigation view controller stopped capturing via viewWilDisappear.
+    [videoCamera startCameraCapture];
+}
+
 - (void)viewDidUnload
 {
     [super viewDidUnload];
