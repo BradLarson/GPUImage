775b1c14195a2ddf69c9ea45a1226bda7380c0ca
Greg Ritter <greg@lolay.com>
2015-04-29 14:39:48 -0700

    Flag some variables that are only used in asserts as __attribute__((unused)).

--- framework/Source/iOS/GPUImageMovieWriter.m
+++ framework/Source/iOS/GPUImageMovieWriter.m
@@ -575,7 +575,7 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
     }
     
 	
-	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	GLenum status __attribute__((unused)) = glCheckFramebufferStatus(GL_FRAMEBUFFER);
     
     NSAssert(status == GL_FRAMEBUFFER_COMPLETE, @"Incomplete filter FBO: %d", status);
 }
