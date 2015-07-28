2b39e5e8851cffeda67dbfa239478fd7c3a8149d
Allan Lavell <alavell@gmail.com>
2015-07-28 18:02:40 -0300

    Remove startTime code from processAudioBuffer, because start time should be set when the video is ready instead. This fixes the blank frames at the beginning of most videos captured from the camera with audio.

--- framework/Source/iOS/GPUImageMovieWriter.m
+++ framework/Source/iOS/GPUImageMovieWriter.m
@@ -373,18 +373,6 @@ NSString *const kGPUImageColorSwizzlingFragmentShaderString = SHADER_STRING
         CFRetain(audioBuffer);
 
         CMTime currentSampleTime = CMSampleBufferGetOutputPresentationTimeStamp(audioBuffer);
-        
-        if (CMTIME_IS_INVALID(startTime))
-        {
-            runSynchronouslyOnContextQueue(_movieWriterContext, ^{
-                if ((audioInputReadyCallback == NULL) && (assetWriter.status != AVAssetWriterStatusWriting))
-                {
-                    [assetWriter startWriting];
-                }
-                [assetWriter startSessionAtSourceTime:currentSampleTime];
-                startTime = currentSampleTime;
-            });
-        }
 
         if (!assetWriterAudioInput.readyForMoreMediaData && _encodingLiveVideo)
         {
