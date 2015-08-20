ba730a037c17e9fc8de95ff1408b43386c9538a2
Graham McDermott <graham@eyegroove.com>
2015-08-19 23:26:57 -0700

    Added ability to override default YUV to RGB conversion matrices

--- framework/Source/GPUImageVideoCamera.m
+++ framework/Source/GPUImageVideoCamera.m
@@ -5,25 +5,47 @@
 // Color Conversion Constants (YUV to RGB) including adjustment from 16-235/16-240 (video range)
 
 // BT.601, which is the standard for SDTV.
-const GLfloat kColorConversion601[] = {
+GLfloat kColorConversion601Default[] = {
     1.164,  1.164, 1.164,
     0.0, -0.392, 2.017,
     1.596, -0.813,   0.0,
 };
 
+// BT.601 full range (ref: http://www.equasys.de/colorconversion.html)
+GLfloat kColorConversion601FullRangeDefault[] = {
+    1.0,    1.0,    1.0,
+    0.0,    -0.343, 1.765,
+    1.4,    -0.711, 0.0,
+};
+
 // BT.709, which is the standard for HDTV.
-const GLfloat kColorConversion709[] = {
+GLfloat kColorConversion709Default[] = {
     1.164,  1.164, 1.164,
     0.0, -0.213, 2.112,
     1.793, -0.533,   0.0,
 };
 
-// BT.601 full range (ref: http://www.equasys.de/colorconversion.html)
-const GLfloat kColorConversion601FullRange[] = {
-    1.0,    1.0,    1.0,
-    0.0,    -0.343, 1.765,
-    1.4,    -0.711, 0.0,
-};
+
+GLfloat *kColorConversion601 = kColorConversion601Default;
+GLfloat *kColorConversion601FullRange = kColorConversion601FullRangeDefault;
+GLfloat *kColorConversion709 = kColorConversion709Default;
+
+void setColorConversion601( GLfloat conversionMatrix[9] )
+{
+    kColorConversion601 = conversionMatrix;
+}
+
+void setColorConversion601FullRange( GLfloat conversionMatrix[9] )
+{
+    kColorConversion601FullRange = conversionMatrix;
+}
+
+void setColorConversion709( GLfloat conversionMatrix[9] )
+{
+    kColorConversion709 = conversionMatrix;
+}
+
+
 
 NSString *const kGPUImageYUVVideoRangeConversionForRGFragmentShaderString = SHADER_STRING
 (
