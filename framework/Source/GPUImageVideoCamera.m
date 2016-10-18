e797b29f58ea1b578579913f7e39b82c8ee58a09
Christophe de Dinechin <cdedinechin@dxo.com>
2016-10-18 20:01:56 +0200

    [IMAGE-81] Fake HDR by adjusting the YUV conversion parameters

--- framework/Source/GPUImageVideoCamera.m
+++ framework/Source/GPUImageVideoCamera.m
@@ -150,60 +150,12 @@ void setColorConversion709( GLfloat conversionMatrix[9] )
     {
         [videoOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_32BGRA] forKey:(id)kCVPixelBufferPixelFormatTypeKey]];
     }
-    
-    runSynchronouslyOnVideoProcessingQueue(^{
-        
-        if (captureAsYUV)
-        {
-            [GPUImageContext useImageProcessingContext];
-            //            if ([GPUImageContext deviceSupportsRedTextures])
-            //            {
-            //                yuvConversionProgram = [[GPUImageContext sharedImageProcessingContext] programForVertexShaderString:kGPUImageVertexShaderString fragmentShaderString:kGPUImageYUVVideoRangeConversionForRGFragmentShaderString];
-            //            }
-            //            else
-            //            {
-            if (isFullYUVRange)
-            {
-                yuvConversionProgram = [[GPUImageContext sharedImageProcessingContext] programForVertexShaderString:kGPUImageVertexShaderString fragmentShaderString:kGPUImageYUVFullRangeConversionForLAFragmentShaderString];
-            }
-            else
-            {
-                yuvConversionProgram = [[GPUImageContext sharedImageProcessingContext] programForVertexShaderString:kGPUImageVertexShaderString fragmentShaderString:kGPUImageYUVVideoRangeConversionForLAFragmentShaderString];
-            }
 
-            //            }
-            
-            if (!yuvConversionProgram.initialized)
-            {
-                [yuvConversionProgram addAttribute:@"position"];
-                [yuvConversionProgram addAttribute:@"inputTextureCoordinate"];
-                
-                if (![yuvConversionProgram link])
-                {
-                    NSString *progLog = [yuvConversionProgram programLog];
-                    NSLog(@"Program link log: %@", progLog);
-                    NSString *fragLog = [yuvConversionProgram fragmentShaderLog];
-                    NSLog(@"Fragment shader compile log: %@", fragLog);
-                    NSString *vertLog = [yuvConversionProgram vertexShaderLog];
-                    NSLog(@"Vertex shader compile log: %@", vertLog);
-                    yuvConversionProgram = nil;
-                    NSAssert(NO, @"Filter shader link failed");
-                }
-            }
-            
-            yuvConversionPositionAttribute = [yuvConversionProgram attributeIndex:@"position"];
-            yuvConversionTextureCoordinateAttribute = [yuvConversionProgram attributeIndex:@"inputTextureCoordinate"];
-            yuvConversionLuminanceTextureUniform = [yuvConversionProgram uniformIndex:@"luminanceTexture"];
-            yuvConversionChrominanceTextureUniform = [yuvConversionProgram uniformIndex:@"chrominanceTexture"];
-            yuvConversionMatrixUniform = [yuvConversionProgram uniformIndex:@"colorConversionMatrix"];
-            
-            [GPUImageContext setActiveShaderProgram:yuvConversionProgram];
-            
-            glEnableVertexAttribArray(yuvConversionPositionAttribute);
-            glEnableVertexAttribArray(yuvConversionTextureCoordinateAttribute);
-        }
-    });
-    
+    if (captureAsYUV)
+    {
+        [self setYUVConversionProgram:nil];
+    }
+
     [videoOutput setSampleBufferDelegate:self queue:cameraProcessingQueue];
 	if ([_captureSession canAddOutput:videoOutput])
 	{
@@ -1058,5 +1010,64 @@ void setColorConversion709( GLfloat conversionMatrix[9] )
     _horizontallyMirrorRearFacingCamera = newValue;
     [self updateOrientationSendToTargets];
 }
+
+- (void)setYUVConversionProgram:(NSString *)conversion
+{
+    if (conversion == nil)
+    {
+        conversion = isFullYUVRange ? kGPUImageYUVFullRangeConversionForLAFragmentShaderString : kGPUImageYUVVideoRangeConversionForLAFragmentShaderString;
+    }
+
+    if (captureAsYUV)
+    {
+        runSynchronouslyOnVideoProcessingQueue(^{
+
+          [GPUImageContext useImageProcessingContext];
+          yuvConversionProgram = [[GPUImageContext sharedImageProcessingContext]  programForVertexShaderString:kGPUImageVertexShaderString fragmentShaderString:conversion];
+
+          if (!yuvConversionProgram.initialized)
+          {
+              [yuvConversionProgram addAttribute:@"position"];
+              [yuvConversionProgram addAttribute:@"inputTextureCoordinate"];
+
+              if (![yuvConversionProgram link])
+              {
+                  NSString *progLog = [yuvConversionProgram programLog];
+                  NSLog(@"Program link log: %@", progLog);
+                  NSString *fragLog = [yuvConversionProgram fragmentShaderLog];
+                  NSLog(@"Fragment shader compile log: %@", fragLog);
+                  NSString *vertLog = [yuvConversionProgram vertexShaderLog];
+                  NSLog(@"Vertex shader compile log: %@", vertLog);
+                  yuvConversionProgram = nil;
+                  NSAssert(NO, @"Filter shader link failed");
+              }
+          }
+
+          yuvConversionPositionAttribute          = [yuvConversionProgram attributeIndex:@"position"];
+          yuvConversionTextureCoordinateAttribute = [yuvConversionProgram attributeIndex:@"inputTextureCoordinate"];
+          yuvConversionLuminanceTextureUniform    = [yuvConversionProgram uniformIndex:@"luminanceTexture"];
+          yuvConversionChrominanceTextureUniform  = [yuvConversionProgram uniformIndex:@"chrominanceTexture"];
+          yuvConversionMatrixUniform              = [yuvConversionProgram uniformIndex:@"colorConversionMatrix"];
+
+          [GPUImageContext setActiveShaderProgram:yuvConversionProgram];
+
+          glEnableVertexAttribArray(yuvConversionPositionAttribute);
+          glEnableVertexAttribArray(yuvConversionTextureCoordinateAttribute);
+
+        });
+    }
+}
+
+- (void) setYUVConversionProgramParameters:(void(^)(GLProgram *))block
+{
+    if (captureAsYUV && block)
+    {
+        runSynchronouslyOnVideoProcessingQueue(^{
+            [GPUImageContext useImageProcessingContext];
+            block(yuvConversionProgram);
+        });
+    }
+}
+
 
 @end
