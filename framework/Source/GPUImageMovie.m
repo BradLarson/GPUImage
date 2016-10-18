7aaebea92e5a6ee5cb9c20880fdbc2cf2dff2130
zhangwx <worthyzhang@gmail.com>
2016-10-18 17:00:59 +0800

    fix: avoid process movie on the main thread

--- framework/Source/GPUImageMovie.m
+++ framework/Source/GPUImageMovie.m
@@ -166,7 +166,9 @@
     }
     if(self.url == nil)
     {
-      [self processAsset];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self processAsset];
+    });
       return;
     }
     
