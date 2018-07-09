4306fc21bc897747aa90b2d3f4e3a6662636edfa
Alik Aslanyan <cplusplus256@gmail.com>
2018-07-09 14:10:17 +0400

    Fix zero division

--- framework/Source/GPUImageHighlightShadowFilter.m
+++ framework/Source/GPUImageHighlightShadowFilter.m
@@ -14,7 +14,7 @@ const mediump vec3 luminanceWeighting = vec3(0.3, 0.3, 0.3);
 void main()
 {
 	lowp vec4 source = texture2D(inputImageTexture, textureCoordinate);
-	mediump float luminance = dot(source.rgb, luminanceWeighting);
+	mediump float luminance = dot(source.rgb, luminanceWeighting) + 0.001;
 
 	mediump float shadow = clamp((pow(luminance, 1.0/(shadows+1.0)) + (-0.76)*pow(luminance, 2.0/(shadows+1.0))) - luminance, 0.0, 1.0);
 	mediump float highlight = clamp((1.0 - (pow(1.0-luminance, 1.0/(2.0-highlights)) + (-0.8)*pow(1.0-luminance, 2.0/(2.0-highlights)))) - luminance, -1.0, 0.0);
