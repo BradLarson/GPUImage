03c8d44960488fa83db7563e7b077ad5f3717feb
Allen Lin <allenlinli@gmail.com>
2019-01-26 11:43:13 +0800

    feat: add function `setupFilters` for GPUImageFilterGroup

--- framework/Source/GPUImageFilterGroup.h
+++ framework/Source/GPUImageFilterGroup.h
@@ -1,6 +1,8 @@
 #import "GPUImageOutput.h"
 #import "GPUImageFilter.h"
 
+typedef GPUImageOutput<GPUImageInput> FilterOutputType;
+
 @interface GPUImageFilterGroup : GPUImageOutput <GPUImageInput>
 {
     NSMutableArray *filters;
@@ -10,6 +12,8 @@
 @property(readwrite, nonatomic, strong) GPUImageOutput<GPUImageInput> *terminalFilter;
 @property(readwrite, nonatomic, strong) NSArray *initialFilters;
 @property(readwrite, nonatomic, strong) GPUImageOutput<GPUImageInput> *inputFilterToIgnoreForUpdates; 
+
++ (instancetype)filterGroupWithFilters:(NSArray <FilterOutputType *> *)filters;
 
 // Filter management
 - (void)addFilter:(GPUImageOutput<GPUImageInput> *)newFilter;
